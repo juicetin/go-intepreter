340c02e6275c540e0ae167aadcb17722ffab25a4
Justin Ting <github@sl.justint.ing>
2025-07-11 18:59:03 +1000

    show parsing errors, up to 2.5 - parsing return statements

--- ast/ast.go
+++ ast/ast.go
@@ -34,8 +34,11 @@ type LetStatement struct {
 	Value Expression
 }
 
-func (ls *LetStatement) statementNode()       {}
-func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
 
 type Identifier struct {
 	Token token.Token // the token.IDENT token
